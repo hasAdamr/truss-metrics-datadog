b100c7f701cb6f93e1bb46ac91f551f7f9765d71
Adam Ryman <adamr@tune.com>
2017-05-24 16:02:52 -0700

    Implement methods

--- metrics-service/handlers/handlers.go
+++ metrics-service/handlers/handlers.go
@@ -1,7 +1,10 @@
 package handlers
 
 import (
+	"errors"
 	"golang.org/x/net/context"
+	"math/rand"
+	"time"
 
 	pb "github.com/hasAdamr/truss-metrics-datadog/metrics-service"
 )
@@ -16,20 +19,29 @@ type metricsService struct{}
 // Fast implements Service.
 func (s metricsService) Fast(ctx context.Context, in *pb.Empty) (*pb.Empty, error) {
 	var resp pb.Empty
-	resp = pb.Empty{}
+	random := rand.Intn(100)
+	if random > 90 {
+		time.Sleep(time.Second * time.Duration(2))
+	}
 	return &resp, nil
 }
 
 // Slow implements Service.
 func (s metricsService) Slow(ctx context.Context, in *pb.Empty) (*pb.Empty, error) {
 	var resp pb.Empty
-	resp = pb.Empty{}
+	random := rand.Intn(5) + 3
+	if random != 0 {
+		time.Sleep(time.Second * time.Duration(random))
+	}
 	return &resp, nil
 }
 
 // RandomError implements Service.
 func (s metricsService) RandomError(ctx context.Context, in *pb.Empty) (*pb.Empty, error) {
-	var resp pb.Empty
-	resp = pb.Empty{}
-	return &resp, nil
+	random := rand.Intn(2)
+	if random == 0 {
+		return &pb.Empty{}, nil
+	}
+	time.Sleep(time.Second * time.Duration(random))
+	return nil, errors.New("sometimes things go wrong, not this time though, this is a test")
 }
