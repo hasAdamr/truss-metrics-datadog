74d3a9eb4d4d29446d797ff38e526343bcd1f6e9
Adam Ryman <adamr@tune.com>
2017-06-14 14:44:07 -0700

    Update handler to endpoint

--- metrics-service/middlewares/endpoints.go
+++ metrics-service/middlewares/endpoints.go
@@ -27,20 +27,22 @@ func WrapEndpoints(in svc.Endpoints) svc.Endpoints {
 	go dsd.SendLoop(ticker.C, "udp", dogStatsDAddr)
 
 	// Pass in LabeledMiddlewares you want applied to every endpoint.
-	// These middlewares get passed the handlers name as their first argument when applied.
+	// These middlewares get passed the endpoint name as their first argument when applied.
 	// This can be used to write generic metric gathering middlewares that can
-	// report the handler name for free.
+	// report the endpoint name for free.
 	in.WrapAllLabeledExcept(requestLatency(dsd.NewHistogram("requestLatency.", 1)))
 	in.WrapAllLabeledExcept(errorCounter(dsd.NewCounter("errorCount.", 1)))
 
 	return in
 }
 
+// requestLatency is a LebeledMiddleware, reporting the request time of and
+// endpoint along with its name
 func requestLatency(h metrics.Histogram) svc.LabeledMiddleware {
 	return func(name string, in endpoint.Endpoint) endpoint.Endpoint {
 		return func(ctx context.Context, req interface{}) (interface{}, error) {
 			defer func(begin time.Time) {
-				h.With("handler", name).Observe(time.Since(begin).Seconds())
+				h.With("endpoint", name).Observe(time.Since(begin).Seconds())
 			}(time.Now())
 			return in(ctx, req)
 		}
@@ -48,15 +50,15 @@ func requestLatency(h metrics.Histogram) svc.LabeledMiddleware {
 }
 
 // errorCounter is a LabeledMiddleware, when applied with WrapAllLabeledExcept
-// name will be populated with the handler name, and such this middleware will
-// report errors to the metric provider with the handler name. Feel free to
+// name will be populated with the endpoints name, and such this middleware will
+// report errors to the metric provider with the endpoints name. Feel free to
 // remove this example middleware
 func errorCounter(errCount metrics.Counter) svc.LabeledMiddleware {
 	return func(name string, in endpoint.Endpoint) endpoint.Endpoint {
 		return func(ctx context.Context, req interface{}) (interface{}, error) {
 			resp, err := in(ctx, req)
 			if err != nil {
-				errCount.With("handler", name).Add(1)
+				errCount.With("endpoint", name).Add(1)
 			}
 			return resp, err
 		}
